7ccb1c4c7da62ce4ca5d84fbcc7c077e4b724968
CarterStevens1 <CarterStevens1@users.noreply.github.com>
2021-06-28 16:33:48 +0100

    need to fix theme changes

--- script.js
+++ script.js
@@ -1,4 +1,88 @@
-"use strict";
+let radios = document.querySelectorAll('input[type="radio"]');
+
+let theme01 = {
+  colorPrimary: "hsl(222, 26%, 31%)",
+  colorScreen: "hsl(224, 36%, 15%)",
+  colorKeybackground: "hsl(223, 31%, 20%)",
+  colorFunctionKey: "hsl(225, 21%, 49%)",
+  colorFuntionkeyShadow: "hsl(224, 28%, 35%)",
+  colorRedbuttons: "hsl(6, 63%, 50%)",
+  colorRedbuttonsShadow: "hsl(6, 70%, 34%)",
+  colorNumbers: "hsl(30, 25%, 89%)",
+  colorNumbersShadow: "hsl(28, 16%, 65%)",
+  colorText: "hsl(221, 14%, 31%)",
+  colorWhite: "hsl(0, 0, 100%)",
+};
+let theme02 = {
+  colorPrimary: "hsl(0, 0%, 90%)",
+  $colorScreen: "hsl(0, 0%, 93%)",
+  colorKeybackground: "hsl(0, 5%, 81%))",
+  colorFunctionKey: "hsl(185, 42%, 37%))",
+  colorFuntionkeyShadow: "hsl(185, 58%, 25%)",
+  $colorRedbuttons: "hsl(25, 98%, 40%)",
+  colorRedbuttonsShadow: "hsl(25, 99%, 27%)",
+  colorNumbers: "hsl(45, 7%, 89%)",
+  colorNumbersShadow: "hsl(35, 11%, 61%)",
+  colorText: "hsl(60, 10%, 19%))",
+  colorWhite: "hsl(0, 0, 100%)",
+};
+let theme03 = {
+  colorPrimary: "hsl(222, 26%, 31%)",
+  colorScreen: "hsl(224, 36%, 15%)",
+  colorKeybackground: "hsl(223, 31%, 20%)",
+  colorFunctionKey: "hsl(225, 21%, 49%)",
+  colorFuntionkeyShadow: "hsl(224, 28%, 35%)",
+  colorRedbuttons: "hsl(6, 63%, 50%)",
+  colorRedbuttonsShadow: "hsl(6, 70%, 34%)",
+  colorNumbers: "hsl(30, 25%, 89%)",
+  colorNumbersShadow: "hsl(28, 16%, 65%)",
+  colorText: "hsl(221, 14%, 31%)",
+  $colorWhite: "hsl(0, 0, 100%)",
+};
+
+let properties = [
+  "$colorPrimary",
+  "$colorScreen",
+  "$colorKeybackground",
+  "$colorFunctionKey",
+  "$colorFuntionkeyShadow",
+  "$colorRedbuttons",
+  "$colorRedbuttonsShadow",
+  "$colorNumbers",
+  "$colorNumbersShadow",
+  "$colorText",
+  "$colorWhite",
+];
+
+radios.forEach((radio) => {
+  radio.addEventListener("click", (e) => {
+    let target = e.target.id;
+    let check = e.target.checked;
+    switch (target) {
+      case "theme-1":
+        setTheme(theme01);
+        break;
+      case "theme-2":
+        setTheme(theme02);
+        break;
+      case "theme-3":
+        setTheme(theme03);
+        break;
+
+      default:
+        break;
+    }
+  });
+});
+
+function setTheme(theme) {
+  let styleTarget = document.documentElement.style;
+
+  properties.forEach((prop) => {
+    let withOutDash = prop.slice(2);
+    styleTarget.setProperty(prop, theme[`${withOutDash}`]);
+  });
+}
 
 const calculator = {
   displaysValue: "0",
